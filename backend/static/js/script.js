39d9cef36b8e75dddada1687fb33a949630b11dc
emilebuzz <61750264+emilebuzz@users.noreply.github.com>
2021-05-10 14:55:42 +0200

    Updated display of elements

--- backend/static/js/script.js
+++ backend/static/js/script.js
@@ -17,15 +17,22 @@ function display(hit){
     tr.appendChild(td);
 
     var td = document.createElement('td');
-    td.appendChild(document.createTextNode(hit.url));
+    var a = document.createElement('a');
+    a.href = hit.url;
+    a.target="_blank";
+    a.appendChild(document.createTextNode("Go to code"));
+    td.appendChild(a);
     tr.appendChild(td);
 
     var td = document.createElement('td');
-    td.appendChild(document.createTextNode(hit.method_or_class));
-    tr.appendChild(td);
-
-    var td = document.createElement('td');
-    td.appendChild(document.createTextNode(hit.codeblock));
+    for (j = 0; j < hit.methods.length; j++) {
+        var p = document.createElement('p');
+        var pre = document.createElement('pre');
+        pre.style = "word-wrap: break-word; white-space: pre-wrap;"
+        pre.appendChild(document.createTextNode(hit.methods[j]));
+        p.appendChild(pre);
+        td.append(p);
+    }
     tr.appendChild(td);
 
     results.appendChild(tr);
